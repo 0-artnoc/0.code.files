f8fd4d8610f3d23b11551a311c5c6a2a4539b7dd
Joe Berria <joeberria@gmail.com>
2019-09-10 20:44:40 -0700

    minor settings update to path and vscode settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 set -g -x fish_greeting 'Welcome Joe'
 
 set -gx LS_COLORS "ow=1;34:tw=1;34:"
-set -gx PATH $HOME/.cargo/bin $PATH
+set -gx PATH $HOME/.cargo/bin /usr/local/go/bin $PATH
+set -gx DISPLAY :0
